fd2e46f769eb28502dd275ac94effd3cc33af685
Bai Omar <bai.laye22@bathspa.ac.uk>
2024-02-04 21:24:08 +0000

    Ayy we finally got it working, onto the final page

--- PokeBATTLE/src/ofApp.cpp
+++ PokeBATTLE/src/ofApp.cpp
@@ -20,6 +20,40 @@ void ofApp::setup() {
     Click2Play.load("Click2Play.png");
     TopRightBtn.load("Icon_Close.png");
     DifficultyBtn.load("Icon_Diffculty_Easy.png");
+    PokeBall.load("sprites/PokeBall.png");
+
+    ofxJSONElement TempAPI;
+    string APIstr;
+    string tempName; int tempNum; 
+    string tempPokeType1, tempPokeType2;
+
+    int MaxSelection = 100;
+    Columns = 6; Rows = 5;
+    Gap = 5; StartX = 65; StartY = 400;
+    FrameX = (((80 * ScreenX) / 100) - (Gap * (Columns - 1))) / Columns; //Allocated space - Gaps / Number of Elements
+    FrameY = (((70 * ScreenY) / 100) - (Gap * (Rows - 1))) / Rows;
+
+    Offset = 0; Limit = Columns * Rows;
+    PokeAPI.open("https://pokeapi.co/api/v2/evolution-chain/?offset=0&limit=" + to_string(MaxSelection));
+    cout << PokeAPI.getRawString() << endl;
+    for (int i = 0; i < min(PokeAPI["count"].asInt(), MaxSelection); i++) {
+        tempPokeType2 = "none";
+        TempAPI.open(PokeAPI["results"][i]["url"].asString());
+        APIstr = TempAPI["chain"]["species"]["url"].asString();
+
+        APIstr.erase(APIstr.end() - 1);
+        auto isoNum = APIstr.rfind("/");
+        APIstr = APIstr.substr(isoNum+1);
+        TempAPI.open("https://pokeapi.co/api/v2/pokemon/" + APIstr);
+
+        tempName = TempAPI["name"].asString();
+        cout << i << " - " << APIstr << endl;
+        tempNum = stoi(APIstr);
+        tempPokeType1 = TempAPI["types"][0]["type"]["name"].asString();
+        if (TempAPI["types"].size() > 1) tempPokeType2 = TempAPI["types"][1]["type"]["name"].asString();
+        Pokemon tempPokemon(tempName, tempNum-1, tempPokeType1, tempPokeType2);
+        StarterPoke.push_back(tempPokemon);
+    }
 }
 
 //--------------------------------------------------------------
@@ -29,111 +63,17 @@ void ofApp::update() {
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-    ofRectangle Button;
     ButtonsCont.clear();
 
     switch (CurrPage) {
     case Pages::MainMenu:
-        Accent1.set(255, 203, 5);
-        Accent2.set(50, 105, 177);
-        DrawBG(Accent1, Accent2);
-
-        
-        StaticImgLoader.draw(55, 90, 1170, 255);
-        
-        Click2Play.draw(330, 380, 620, 220);
-        Button.set(330, 380, 620, 220);
-        ButtonsCont.push_back(make_pair(AllButtons::Click2Play, Button));
-
-        TopRightBtn.draw((ScreenX - TopRightBtn.getWidth() - 15), 15, TopRightBtn.getWidth(), 50);
-        Button.set((ScreenX - TopRightBtn.getWidth() - 15), 15, TopRightBtn.getWidth(), 50);
-        ButtonsCont.push_back(make_pair(AllButtons::TopRightBtn, Button));
-
-        DifficultyBtn.draw(1060, 465, 200, 65);
-        Button.set(1060, 465, 200, 65);
-        ButtonsCont.push_back(make_pair(AllButtons::DifficultyBtn, Button));
+        DrawPage1();
         break;
     case Pages::PickPokemon: //TODO: move methods for pages into their own function
-        Accent1.set(75, 75, 75);
-        Accent2.set(75, 75, 75);
-        
-        DrawBG(Accent1, Accent2);
-        
-        StaticImgLoader.load("PokeBattle_Logo.png");
-        StaticImgLoader.draw(50,10,585,135);
-        
-        TopRightBtn.draw((ScreenX - TopRightBtn.getWidth() - 15), 15, TopRightBtn.getWidth(), 50);
-        Button.set((ScreenX - TopRightBtn.getWidth() - 15), 15, TopRightBtn.getWidth(), 50);
-        ButtonsCont.push_back(make_pair(AllButtons::TopRightBtn, Button));
-
-        ofSetColor(255, 255, 255);
-        Font_Selector.load("Jura-Medium.ttf", 24);
-        Font_Selector.drawString("CHOOSE YOUR TEAM", 319, 144);
-
-        ofSetColor(255, 255, 255);
-        Font_Selector.load("TurpisItalic.ttf", 40);
-        Font_Selector.drawString("Pokemon-Left:", 750, 140);
-
-        ofSetColor(222, 90, 58);
-        Font_Selector.load("TurpisItalic.ttf", 80);
-        Font_Selector.drawString(to_string(PokemonLeft), 1126, 170);
-
-        ofSetColor(0);
-        ofFill();
-        ofDrawRectRounded(10, 155, 1105, 12, 6);
-        ofDrawRectangle(20, 155, 1095, 12);
-        ofDrawRectRounded(1205, 155, 65, 12, 6);
-        ofDrawRectangle(1205, 155, 55, 12);
-        ofDrawRectRounded(1050, 180, 300, 700, 3);
-        
-        
-        ofSetColor(255);
-        ofFill();
-        ofDrawRectRounded(13, 158, 1099, 6, 4);
-        ofDrawRectangle(23, 158, 1089, 6);
-        ofDrawRectRounded(1208, 158, 59, 6, 4);
-        ofDrawRectangle(1208, 158, 49, 6);
-        ofDrawRectRounded(1053, 183, 300, 700, 3);
-
-        ofPushMatrix();
-            ofTranslate(10, 180);
-            for (int i = 0; i < Rows; i++) {
-                for (int j = 0; j < Columns; j++) {
-                    string PokeLocation = "sprites/" + to_string((i*Columns+j)+Offset) + "/front.png";
-
-                    Accent1.set(255);
-                    auto it = find(PokeSelected.begin(), PokeSelected.end(), (i * Columns + j) + Offset);
-                    if (it != PokeSelected.end()) Accent1.set(0, 130, 255);
-
-                    StaticImgLoader.load(PokeLocation);
-                    ofSetColor(0);
-                    ofFill();
-                    ofDrawRectRounded((FrameX+Gap)*j, (FrameY + Gap) * i, FrameX, FrameY, 4);
-                    ofSetColor(Accent1);
-                    ofFill();
-                    ofDrawRectRounded(((FrameX + Gap) * j)+3, ((FrameY + Gap) * i)+3, FrameX - 6, FrameY - 6, 3);
-                    ofSetColor(255);
-                    StaticImgLoader.draw((FrameX + Gap) * j, (FrameY + Gap) * i, FrameX, FrameY);
-                }
-            }
-        ofPopMatrix();
-        Button.set(10, 180, (80 * ScreenX) / 100, (70 * ScreenY) / 100);
-        ButtonsCont.push_back(make_pair(AllButtons::PokeSelectGrid, Button));
-
-        ofPushMatrix();
-            ofTranslate(40,690);
-            ofScale(-1, 1);
-            StaticImgLoader.load("Icon_Play.png");
-            StaticImgLoader.draw(0, 0, 25, 25);
-        ofPopMatrix();
-        Button.set(40, 690, 25, 25);
-        ButtonsCont.push_back(make_pair(AllButtons::PrevPage, Button));
-
-        StaticImgLoader.draw(1000, 690, 25, 25);
-        Button.set(1000, 690, 25, 25);
-        ButtonsCont.push_back(make_pair(AllButtons::NextPage, Button));
+        DrawPage2();
         break;
     case Pages::BattleScene:
+        DrawPage3();
         break;
     }
     ofSetColor(255); //reset colours after every page
@@ -175,9 +115,140 @@ void ofApp::DrawBG(ofColor Col1, ofColor Col2) {
     Gradient.draw(); //draw mesh
 }
 
-ofRectangle ofApp::DrawImage(ofImage aImage, int widthPercent, int heightPercent, char alignX, char alignY, int* PaddingPtr) {
-    ofRectangle SOmething;
-    return SOmething;
+void ofApp::DrawPage1() {
+    ofRectangle Button;
+    Accent1.set(255, 203, 5);
+    Accent2.set(50, 105, 177);
+    DrawBG(Accent1, Accent2);
+
+    StaticImgLoader.draw(55, 90, 1170, 255);
+
+    Click2Play.draw(330, 380, 620, 220);
+    Button.set(330, 380, 620, 220);
+    ButtonsCont.push_back(make_pair(AllButtons::Click2Play, Button));
+
+    TopRightBtn.draw((ScreenX - TopRightBtn.getWidth() - 15), 15, TopRightBtn.getWidth(), 50);
+    Button.set((ScreenX - TopRightBtn.getWidth() - 15), 15, TopRightBtn.getWidth(), 50);
+    ButtonsCont.push_back(make_pair(AllButtons::TopRightBtn, Button));
+
+    DifficultyBtn.draw(1060, 465, 200, 65);
+    Button.set(1060, 465, 200, 65);
+    ButtonsCont.push_back(make_pair(AllButtons::DifficultyBtn, Button));
+}
+
+void ofApp::DrawPage2() {
+    ofRectangle Button;
+    Accent1.set(75, 75, 75);
+    Accent2.set(75, 75, 75);
+    DrawBG(Accent1, Accent2);
+
+    StaticImgLoader.load("PokeBattle_Logo.png");
+    StaticImgLoader.draw(50, 10, 585, 135);
+
+    TopRightBtn.draw((ScreenX - TopRightBtn.getWidth() - 15), 15, TopRightBtn.getWidth(), 50);
+    Button.set((ScreenX - TopRightBtn.getWidth() - 15), 15, TopRightBtn.getWidth(), 50);
+    ButtonsCont.push_back(make_pair(AllButtons::TopRightBtn, Button));
+
+    ofSetColor(255);
+    Font_Selector.load("Jura-Medium.ttf", 24);
+    Font_Selector.drawString("CHOOSE YOUR TEAM", 319, 144);
+
+    ofSetColor(255);
+    Font_Selector.load("TurpisItalic.ttf", 40);
+    Font_Selector.drawString("Pokemon-Left:", 750, 140);
+
+    ofSetColor(222, 90, 58);
+    Font_Selector.load("TurpisItalic.ttf", 80);
+    Font_Selector.drawString(to_string(PokemonLeft), 1126, 170);
+
+    ofSetColor(0);
+    ofFill();
+    ofDrawRectRounded(10, 155, 1105, 12, 6);
+    ofDrawRectangle(20, 155, 1095, 12);
+    ofDrawRectRounded(1205, 155, 65, 12, 6);
+    ofDrawRectangle(1205, 155, 55, 12);
+    ofDrawRectRounded(1050, 180, 300, 700, 3);
+
+
+    ofSetColor(255);
+    ofFill();
+    ofDrawRectRounded(13, 158, 1099, 6, 4);
+    ofDrawRectangle(23, 158, 1089, 6);
+    ofDrawRectRounded(1208, 158, 59, 6, 4);
+    ofDrawRectangle(1208, 158, 49, 6);
+    ofDrawRectRounded(1053, 183, 300, 700, 3);
+
+    ofPushMatrix();
+    ofTranslate(1050, 180);
+    for (int i = 0; i < PokeSelected.size(); i++) {
+        string PokeLocation = "sprites/" + to_string(PokeSelected[i]) + "/back.png";
+        StaticImgLoader.load(PokeLocation);
+        PokeBall.draw(5, ((ScreenY - 180) / 6) * i+10, (ScreenY - 180) / 6, (ScreenY - 180) / 6);
+        StaticImgLoader.draw(60, ((ScreenY - 180) / 6) * i - 25, 160, 160);
+    }
+    ofPopMatrix();
+    Button.set(1050, 180, ScreenX - 1050, ScreenY - 180);
+    ButtonsCont.push_back(make_pair(AllButtons::PokeSelected, Button));
+
+    ofPushMatrix();
+    ofTranslate(10, 180);
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Columns; j++) {
+            if ((i * Columns + j + 1 + Offset) > StarterPoke.size()) break;
+            string PokeLocation = "sprites/" + to_string(StarterPoke[i * Columns + j + Offset].PokeNum) + "/front.png";
+
+            Accent1.set(255);
+            auto it = find(PokeSelected.begin(), PokeSelected.end(), StarterPoke[i * Columns + j + Offset].PokeNum);
+            if (it != PokeSelected.end()) Accent1.set(0, 130, 255);
+
+            StaticImgLoader.load(PokeLocation);
+            ofSetColor(0);
+            ofFill();
+            ofDrawRectRounded((FrameX + Gap) * j, (FrameY + Gap) * i, FrameX, FrameY, 4);
+            ofSetColor(Accent1);
+            ofFill();
+            ofDrawRectRounded(((FrameX + Gap) * j) + 3, ((FrameY + Gap) * i) + 3, FrameX - 6, FrameY - 6, 3);
+            ofSetColor(255);
+            StaticImgLoader.draw((FrameX + Gap) * j, (FrameY + Gap) * i, FrameX, FrameY);
+        }
+    }
+    ofPopMatrix();
+    Button.set(10, 180, (80 * ScreenX) / 100, (70 * ScreenY) / 100);
+    ButtonsCont.push_back(make_pair(AllButtons::PokeSelectGrid, Button));
+
+    StaticImgLoader.load("Icon_Play.png");
+    if (Offset > 0) {
+        ofPushMatrix();
+        ofTranslate(40, 690);
+        ofScale(-1, 1);
+        StaticImgLoader.draw(0, 0, 25, 25);
+        ofPopMatrix();
+        Button.set(5, 685, 35, 35);
+        ButtonsCont.push_back(make_pair(AllButtons::PrevPage, Button));
+    }
+    if (Offset + Limit < StarterPoke.size()) {
+        StaticImgLoader.draw(1000, 690, 25, 25);
+        Button.set(995, 685, 35, 35);
+        ButtonsCont.push_back(make_pair(AllButtons::NextPage, Button));
+    }
+
+    if (PokemonLeft < 6){
+        ofSetColor(0);
+        ofFill();
+        ofDrawRectRounded(270, 685, 500, FrameY, 5);
+        ofSetColor(0, 130, 255);
+        ofFill();
+        ofDrawRectRounded(273, 688, 494, FrameY, 5);
+        ofSetColor(255);
+        Button.set(270, 685, 500, FrameY);
+        ButtonsCont.push_back(make_pair(AllButtons::Start, Button));
+        Font_Selector.load("Jura-Medium.ttf", 20);
+        Font_Selector.drawString("START", 480, 715);
+    }
+}
+
+void ofApp::DrawPage3() {
+
 }
 
 //--------------------------------------------------------------
@@ -209,29 +280,11 @@ void ofApp::mousePressed(int x, int y, int button) {
     case AllButtons::none:
         return;
     case AllButtons::Click2Play:
-        
         CurrPage = Pages::PickPokemon;
         TopRightBtn.load("Icon_Back.png");
-        PokemonLeft = 3;
-
-        Columns = 6; Rows = 5;
-        Gap = 5; StartX = 65; StartY = 400;
-        FrameX = (((80 * ScreenX) / 100) - (Gap * (Columns - 1))) / Columns; //Allocated space - Gaps / Number of Elements
-        FrameY = (((70 * ScreenY) / 100) - (Gap * (Rows - 1))) / Rows;
-
-        Offset = 0; Limit = Columns * Rows;
-        APIstr = "https://pokeapi.co/api/v2/evolution-chain/?offset=0&limit=" + to_string(Limit);
-        PokeAPI.open(APIstr);
-        cout << PokeAPI.getRawString() << endl;
-        PokeInPage.clear();
-        for (int i = 0; i < Limit; i++) {
-            TempAPI.open(PokeAPI["results"][i]["url"].asString());
-            TempAPI.open(TempAPI["chain"]["species"]["url"].asString());
-            PokeInPage.push_back(TempAPI["id"].asInt());
-        }
-        for (int i: PokeInPage){
-            cout << i << ", ";
-        }
+        PokemonLeft = 6;
+        PokeSelected.clear();
+        Offset = 0;
         break;
     case AllButtons::TopRightBtn:
         switch (CurrPage) {
@@ -251,7 +304,6 @@ void ofApp::mousePressed(int x, int y, int button) {
         DifficultyOption++;
         switch (DifficultyOption) {
         case 1:
-            //DifficultyBtn.load("Icon_Diffculty_Medium_Alt.png");
             DifficultyBtn.load("Icon_Diffculty_Medium.png");
             break;
         case 2:
@@ -266,53 +318,34 @@ void ofApp::mousePressed(int x, int y, int button) {
     case AllButtons::PrevPage:
         Offset -= Limit;
         if (Offset < 0) Offset = 0;
-        APIstr = "https://pokeapi.co/api/v2/evolution-chain/?offset=" + to_string(Offset) + "& limit = " + to_string(Limit);
-        PokeAPI.open(APIstr);
-        cout << PokeAPI.getRawString() << endl;
-        PokeInPage.clear();
-        for (int i = 0; i < Limit; i++) {
-            TempAPI.open(PokeAPI["results"][i]["url"].asString());
-            TempAPI.open(TempAPI["chain"]["species"]["url"].asString());
-            PokeInPage.push_back(TempAPI["id"].asInt());
-        }
-        for (int i : PokeInPage) {
-            cout << i << ", ";
-        }
-        /*for (int i = 0; i < Limit; i++){
-            TempAPI.open(PokeAPI["results"][i]["url"].asString());
-            APIstr = TempAPI["chain"]["species"]["url"].asString();
-            APIstr.erase(APIstr.end() - 1);
-            auto isoPokeNum = APIstr.rfind('/');
-            APIstr.substr(isoPokeNum + 1);
-            cout << APIstr << ", " << endl;
-        }*/
         break;
     case AllButtons::NextPage:
-        if (Offset + Limit > PokeAPI["count"].asInt()) break;
+        if (Offset + Limit > StarterPoke.size()) break;
         Offset += Limit;
-        APIstr = "https://pokeapi.co/api/v2/evolution-chain/?offset=" + to_string(Offset) + "& limit = " + to_string(Limit);
-        PokeAPI.open(APIstr);
-        cout << PokeAPI.getRawString() << endl;
-        PokeInPage.clear();
-        for (int i = 0; i < Limit; i++) {
-            TempAPI.open(PokeAPI["results"][i]["url"].asString());
-            TempAPI.open(TempAPI["chain"]["species"]["url"].asString());
-            PokeInPage.push_back(TempAPI["id"].asInt());
-        }
-        for (int i : PokeInPage) {
-            cout << i << ", ";
-        }
         break;
     case AllButtons::PokeSelectGrid:
         if (PokeSelected.size() >= 6) {
-            cout << "Selection Full!" << endl;
             if(PokeSelected.size() > 6) PokeSelected.resize(6); //hate to bug out this way
             break;
         }
         x -= 10; y -= 180;
         x /= FrameX; y /= FrameY;
-        cout << "Selected: x-" << x << " | y-" << y << " | PokemonNo-" << ((y * Columns + x) + Offset) << endl;
-        PokeSelected.push_back((y * Columns + x) + Offset);
+        if ((y * Columns + x + 1 + Offset) > StarterPoke.size()) break;
+        PokeSelected.push_back(StarterPoke[y * Columns + x + Offset].PokeNum);
+        PokemonLeft = 6 - PokeSelected.size();
+        break;
+    case AllButtons::PokeSelected:
+        if (PokeSelected.size() == 0) break;
+        y -= 180;
+        y /= ((ScreenY - 180) / 6);
+        if (PokeSelected.size() < y+1) break;
+        PokeSelected.erase(PokeSelected.begin()+y);
+        PokemonLeft = 6 - PokeSelected.size();
+        break;
+    case AllButtons::Start:
+        CurrPage = Pages::BattleScene;
+        TopRightBtn.load("Icon_Pause.png");
+        PokemonLeft = 6;
         break;
     default:
         break;
--- PokeBATTLE/src/ofApp.h
+++ PokeBATTLE/src/ofApp.h
@@ -6,15 +6,47 @@
 #include <algorithm>
 #include <string>
 
+enum class PokeTypes {
+	none,
+	Bug, Dark, Dragon,
+	Electric, Fairy, Fighting,
+	Fire, Flying, Ghost,
+	Grass, Ground, Ice,
+	Normal, Poison, Psychic,
+	Rock, Steel, Water
+};
 
-class MakeButton {
-	ofRectangle MyRect;
+class Pokemon {
 public:
-	MakeButton(ofImage Picture) {
-		
-	};
-	MakeButton(ofRectangle Rectangle) {
-
+	string PokeName;
+	int PokeNum;
+	pair <PokeTypes, PokeTypes> PokeTyping;
+	PokeTypes setTyping(string &Type) {
+		if (Type == "none") return PokeTypes::none;
+		else if (Type == "bug") return PokeTypes::Bug;
+		else if (Type == "dark") return PokeTypes::Dark;
+		else if (Type == "dragon") return PokeTypes::Dragon;
+		else if (Type == "electric") return PokeTypes::Electric;
+		else if (Type == "fairy") return PokeTypes::Fairy;
+		else if (Type == "fighting") return PokeTypes::Fighting;
+		else if (Type == "fire") return PokeTypes::Fire;
+		else if (Type == "flying") return PokeTypes::Flying;
+		else if (Type == "ghost") return PokeTypes::Ghost;
+		else if (Type == "grass") return PokeTypes::Grass;
+		else if (Type == "ground") return PokeTypes::Ground;
+		else if (Type == "ice") return PokeTypes::Ice;
+		else if (Type == "normal") return PokeTypes::Normal;
+		else if (Type == "poison") return PokeTypes::Poison;
+		else if (Type == "psychic") return PokeTypes::Psychic;
+		else if (Type == "rock") return PokeTypes::Rock;
+		else if (Type == "steel") return PokeTypes::Steel;
+		else if (Type == "water") return PokeTypes::Water;
+		else return PokeTypes::none;
+	}
+	Pokemon(string aName, int aNum, string Type1, string Type2) {
+		PokeName = aName;
+		PokeNum = aNum;
+		PokeTyping = make_pair(setTyping(Type1), setTyping(Type2));
 	};
 };
 
@@ -30,9 +62,10 @@ public:
 	void mouseReleased(int x, int y, int button);
 
 	void DrawBG(ofColor Col1, ofColor Col2);
-	ofRectangle DrawImage(ofImage aImage, int width /* = 0 */, int height /* = 0 */, char alignX /* = 'n' */, char alignY /* = 'n' */, int*Padding);
-	// it appears openframeworks doesn't like default values... since they are custom functions ill put the default values whenever i call them with "my defaults" :|
-	
+	void DrawPage1();
+	void DrawPage2();
+	void DrawPage3();
+
 	enum class Pages {
 		MainMenu,
 		PickPokemon,
@@ -47,6 +80,7 @@ public:
 		PrevPage,
 		NextPage,
 		PokeSelectGrid,
+		PokeSelected,
 		Start
 	};
 
@@ -56,7 +90,7 @@ public:
 	int PokemonLeft;
 	int DifficultyOption;
 
-	vector<int> PokeInPage;
+	vector<Pokemon> StarterPoke;
 	vector<int> PokeSelected;
 	int Offset, Limit;
 	int Columns, Rows;
@@ -71,6 +105,7 @@ public:
 	ofImage Click2Play;
 	ofImage TopRightBtn; //top right icon... could serve multiple functions based on page or state
 	ofImage DifficultyBtn;
+	ofImage PokeBall;
 	
 	ofTrueTypeFont Font_Selector;
 	vector< pair<AllButtons, ofRectangle> > ButtonsCont; 
